ca063c28b11f5507348935b24398503911d0b004
Muhammad Muzammil <muzans786@gmail.com>
2026-02-18 12:26:16 +0500

    ADD Soluion of Assignment 01

---
+++ Solutrions/chap_1/app.js
@@ -0,0 +1,18 @@
+// ALert 
+//. Write a script to greet your website visitor using JS alert box.
+// alert("Hello! Wlesome to my website")
+
+//2. Write a script to display following message on your webpage:
+// alert("Error! Please enter a valid password");
+
+//3.
+// alert ("Welcome to JS Land \n Happy Coding!")
+
+//4.
+// alert("Welcome to JS Land")
+// alert ("Happy Coding! \n prevents this page from creating additional dialogs")
+
+//5.
+// alert("Hello... i can run JS through my web browser's console")
+
+//6. 
