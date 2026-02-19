67ee35065a17c048ba3c888077396d103ae877e5
Muhammad Muzammil <muzans786@gmail.com>
2026-02-19 12:37:29 +0500

    Add Solution of Chap 03

---
+++ Solutrions/chap_4/app.js
@@ -0,0 +1,24 @@
+//1.
+var num1 ,num2, num3;
+
+//2.
+//legal variable names
+var num1 =5
+var userName = 'Muhammad Muzammil'
+var gender  ='male'
+var age = 25
+var isMarried = false
+
+//illegal variable names
+// var 1num = 5
+// var user-name = 'Muhammad Muzammil'
+// var gender@  ='male'
+// var age 25 = 25
+// var is Married = false
+
+//3.
+document.writeln("<h1>Rules for naming JS variables</h1>")
+document.writeln("<p>Variable names can only contain numbers, letters, underscores, and dollar signs.</p>")
+document.writeln("<p>Variables must begin with a letter, underscore, or dollar sign.</p>")
+document.writeln("<p>Variables are case sensitive.</p>")
+document.writeln("<p>Variables names should not be JS keywords.</p>")
