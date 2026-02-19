fbd206f39e26a90317dfd937f1cb08e17678d929
Muhammad Muzammil <muzans786@gmail.com>
2026-02-19 12:29:31 +0500

    Add Solution of Chap 03

---
+++ Solutrions/chap _3/app.js
@@ -0,0 +1,19 @@
+//1.
+// var age = 15
+// alert("I am "+ age +" years old")
+
+//2.
+
+//3.
+// var birthYear = 2006 
+// alert(birthYear)
+// document.writeln("My birth year is " + birthYear)
+// document.writeln("Data type of my declared variable is number")
+
+//4.
+// var visitorName = "Muhammad Muzammil"
+// var productTitle = "T-shirt"
+// var quantity = 5
+// document.writeln(visitorName + " ordered " + quantity + " " + productTitle + "(s) on my Clothing store")
+
+
