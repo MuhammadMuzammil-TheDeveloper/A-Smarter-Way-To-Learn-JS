af19ec4097254e4f4eccfa987033340bf6917803
Muhammad Muzammil <muzans786@gmail.com>
2026-02-21 07:15:03 +0500

    Add Solution 0f Chap_5

--- Solutrions/chap_4/app.js
+++ Solutrions/chap_4/app.js
@@ -1,13 +1,13 @@
 //1.
-var num1 ,num2, num3;
+// var num1 ,num2, num3;
 
 //2.
 //legal variable names
-var num1 =5
-var userName = 'Muhammad Muzammil'
-var gender  ='male'
-var age = 25
-var isMarried = false
+// var num1 =5
+// var userName = 'Muhammad Muzammil'
+// var gender  ='male'
+// var age = 25
+// var isMarried = false
 
 //illegal variable names
 // var 1num = 5
@@ -16,9 +16,9 @@ var isMarried = false
 // var age 25 = 25
 // var is Married = false
 
-//3.
-document.writeln("<h1>Rules for naming JS variables</h1>")
-document.writeln("<p>Variable names can only contain numbers, letters, underscores, and dollar signs.</p>")
-document.writeln("<p>Variables must begin with a letter, underscore, or dollar sign.</p>")
-document.writeln("<p>Variables are case sensitive.</p>")
-document.writeln("<p>Variables names should not be JS keywords.</p>")
+// //3.
+// document.writeln("<h1>Rules for naming JS variables</h1>")
+// document.writeln("<p>Variable names can only contain numbers, letters, underscores, and dollar signs.</p>")
+// document.writeln("<p>Variables must begin with a letter, underscore, or dollar sign.</p>")
+// document.writeln("<p>Variables are case sensitive.</p>")
+// document.writeln("<p>Variables names should not be JS keywords.</p>")
