d111d52c2b741e9c29fe1dbad03e284cde1eaf99
Muhammad Muzammil <muzans786@gmail.com>
2026-02-21 21:30:04 +0500

    Add Solution

---
+++ Solutrions/chap_11_to_13/app.js
@@ -0,0 +1,17 @@
+//1.
+var studentName = [];
+studentName[0] = "muhammad-muzammil";
+studentName[1] = "muhammad-ahmed";
+//2.
+var students = new Array();
+students[0] = "Ahmed";
+//3.
+var stringData = ["Ali", "Ahmed"];
+//4.
+var numberData = [12, 13, 14];
+//5.
+var booleanData = [ture, false];
+//6.
+var mixedData = ["ali", 12, true];
+//7.
+var
