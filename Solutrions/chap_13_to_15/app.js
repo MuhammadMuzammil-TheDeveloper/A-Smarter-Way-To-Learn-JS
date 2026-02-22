b0d10a82d5e664a9b4db6588efb43ead8ea82238
Muhammad Muzammil <muzans786@gmail.com>
2026-02-22 08:05:05 +0500

    New Assignmet Solution of Chap 11 till 13

---
+++ Solutrions/chap_13_to_15/app.js
@@ -0,0 +1,19 @@
+//1.
+// var firstName = prompt(`Enter a FirstName `);
+// var lastName = prompt(`Enter a LastName `);
+// alert(firstName + lastName);
+
+//2.
+// var favPhone = prompt(`Enter a Favorite Phone Brand`);
+// document.writeln(`<h1>My Favorite Phone is: ${favPhone}</h1>`);
+// document.writeln(`<h1>My Favorite Phone is: ${favPhone.length}</h1>`);
+
+//3.
+// var city = `Pakistan`;
+// console.log(city.indexOf("n"));
+
+//4.
+// var city = `Pakistan`;
+// console.log(city.lastIndexOf("a"));
+
+//5.
