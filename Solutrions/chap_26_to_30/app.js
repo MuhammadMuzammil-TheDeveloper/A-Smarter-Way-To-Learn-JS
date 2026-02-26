f453d04d6eb8f30a2c9467df5abf4f89757aba04
Muhammad Muzammil <muzans786@gmail.com>
2026-02-26 20:10:29 +0500

    add some more questions

---
+++ Solutrions/chap_26_to_30/app.js
@@ -0,0 +1,22 @@
+// //1.
+// var num = +prompt(`Enter a positive Number`);
+// console.log(num)
+// console.log(Math.round(num))
+// console.log(Math.ceil(num))
+// console.log(Math.floor(num))
+
+//2.
+// var num = +prompt(`Enter a negative Number`);
+// console.log(num)
+// console.log(Math.round(num))
+// console.log(Math.ceil(num))
+// console.log(Math.floor(num))
+
+// //3.
+// var value = Math.abs(-4)
+// console.log(value)
+
+//4.
+var diceVale = Math.random()*6;
+
+console.log(diceVale)
