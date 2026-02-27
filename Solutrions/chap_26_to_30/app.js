6dbf47f8edf117cfc61692982ed3a7c18da11b77
Muhammad Muzammil <muzans786@gmail.com>
2026-02-27 19:42:15 +0500

    Add solution of Chap 26 to 30

--- Solutrions/chap_26_to_30/app.js
+++ Solutrions/chap_26_to_30/app.js
@@ -17,6 +17,33 @@
 // console.log(value)
 
 //4.
-var diceVale = Math.random()*6;
+// var diceValue = Math.floor(Math.random() * 6) + 1;
+// document.write("Dice Value: " + diceValue);
 
-console.log(diceVale)
+//5.
+// var coin = Math.floor(Math.random()*2 ) +1;
+//  document.writeln(`Coin - ${coin}`)
+// if(coin == 1){
+//     document.writeln(`random coin value: HEAD`)
+// }else{
+//     document.writeln(`random coin value: Tails`)
+// }
+
+// //6.
+// var randomNumber = Math.floor(Math.random()*100)+1
+// document.writeln(`random number between 1 and 100: ${randomNumber}`)
+
+//7.
+// var userWeight = prompt(`Enter your Age`)
+// var chect = parseInt(userWeight)
+// var weight = parseFloat(userWeight)
+// document.writeln(`The Weight of the user is ${weight} kilograms.`)
+
+//8.
+var usreInput = +prompt(`Enter a number between 1 and 10`);
+var scerectNumber = Math.floor(Math.random()*10)+1;
+if(usreInput === scerectNumber){
+    document.writeln(`You Win`)
+}else{
+    document.writeln(`Try Again`)
+}
