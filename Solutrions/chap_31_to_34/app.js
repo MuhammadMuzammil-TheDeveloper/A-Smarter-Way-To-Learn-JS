54e8c4b1ed07ec64871314a882e8a028caabb8ed
Muhammad Muzammil <muzans786@gmail.com>
2026-02-28 07:04:55 +0500

    ADD Function

---
+++ Solutrions/chap_31_to_34/app.js
@@ -0,0 +1,37 @@
+//1.
+// console.log(new Date())
+
+//2.
+// var monthName = new Date().toLocaleString('en-US', { month: 'long' });
+// console.log(monthName);
+
+//3.
+// var day= new Date().toLocaleString(`en-US`,{weekday: `long`});
+// console.log(day)
+
+//4. 
+// var day = new Date()
+// var currentDay = day.getDay();
+// if(currentDay == 6 || currentDay==7){
+//     console.log(`Its Fun Day`)
+// }else{
+//     console.log(currentDay)
+// }
+
+//5.
+// var date = new Date()
+// var currentDate = date.getDate();
+// if(currentDate<16){
+//     console.log(`first fifteen day`)
+// }else{
+//     console.log(`last day of month`)
+// }
+
+//6.
+// var date = new Date()
+// var milliSecond = date.getTime()
+// console.log(milliSecond)
+// milliSecond1947 = Math.floor(milliSecond /(1000*60))
+// console.log(milliSecond1947)
+
+//7.
---
+++ Solutrions/chap_35_to_38/app.js
@@ -0,0 +1,29 @@
+// //1.
+// function showCurrentDate(){
+//     var currentDate = new Date();
+//     console.log(currentDate)
+// }
+// showCurrentDate()
+
+//2.
+// function greetUser(){
+// var firstName = prompt(`Entwer a firstName`)
+// var LastName = prompt(`Entwer a LastName`)
+// alert(firstName+LastName)
+// }
+// greetUser()
+
+//3.
+// function sumOf(){
+//   var num1 = +prompt(`Entwer a num1`)
+// var num2 = +prompt(`Enter a num2`)
+// console.log(num1+num2)
+// }
+// sumOf()
+
+//4.
+funcion cal(num1, opt, num2){
+    if(opt == `+`){
+        
+    }
+}
