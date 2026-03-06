4dc2578e6fe712c6a8923483f16f0bf65f884d12
Muhammad Muzammil <muzans786@gmail.com>
2026-03-06 17:50:14 +0500

    ADD API CONFIGuration

---
+++ API/app.js
@@ -0,0 +1,6 @@
+// API SYNTA
+fetch("https://opentdb.com/api.php?amount=5")
+.then(response => response.json())
+.then(data => {
+    console.log(data)
+})
--- QUIZ Application/app.js
+++ QUIZ Application/app.js
@@ -1,194 +1,226 @@
-var quizQuestions = [
-    {
-        id: 1,
-        question: "HTML stands for?",
-        options: [
-            "Hyper Text Markup Language",
-            "Hyper Text Programming Language",
-            "Hyper Text Styling Language",
-            "Hyper Text Scripting Language"
-        ],
-        answer: "Hyper Text Markup Language"
-    },
-    {
-        id: 2,
-        question: "Which HTML tag is used to create a hyperlink?",
-        options: [
-            "a",
-            "link",
-            "href",
-            "hyperlink"
-        ],
-        answer: "a"
-    },
-    {
-        id: 3,
-        question: "Which CSS property is used to change the text color?",
-        options: [
-            "font-color",
-            "text-color",
-            "color",
-            "background-color"
-        ],
-        answer: "color"
-    },
-    {
-        id: 4,
-        question: "Which CSS property controls the size of text?",
-        options: [
-            "text-style",
-            "font-size",
-            "text-size",
-            "font-style"
-        ],
-        answer: "font-size"
-    },
-    {
-        id: 5,
-        question: "Inside which HTML element do we put JavaScript?",
-        options: [
-            "javascript",
-            "js",
-            "script",
-            "code"
-        ],
-        answer: "script"
-    },
-    {
-        id: 6,
-        question: "Which symbol is used for single-line comments in JavaScript?",
-        options: [
-            "//",
-            "/* */",
-            "#",
-        ],
-        answer: "//"
-    },
-    {
-        id: 7,
-        question: "Which JavaScript keyword is used to declare a variable?",
-        options: [
-            "var",
-            "int",
-            "string",
-            "declare"
-        ],
-        answer: "var"
-    },
-    {
-        id: 8,
-        question: "Which CSS property is used to make a website responsive?",
-        options: [
-            "flex",
-            "media-query",
-            "@media",
-            "responsive"
-        ],
-        answer: "@media"
-    },
-    {
-        id: 9,
-        question: "Which method is used to select an element by its ID in JavaScript?",
-        options: [
-            "getElementByClass()",
-            "getElementById()",
-            "querySelectorAll()",
-            "selectById()"
-        ],
-        answer: "getElementById()"
-    },
-    {
-        id: 10,
-        question: "Which HTML attribute is used to define inline styles?",
-        options: [
-            "class",
-            "styles",
-            "style",
-            "font"
-        ],
-        answer: "style"
-    }
-];
+// var quizQuestions = [
+//     {
+//         id: 1,
+//         question: "HTML stands for?",
+//         options: [
+//             "Hyper Text Markup Language",
+//             "Hyper Text Programming Language",
+//             "Hyper Text Styling Language",
+//             "Hyper Text Scripting Language"
+//         ],
+//         answer: "Hyper Text Markup Language"
+//     },
+//     {
+//         id: 2,
+//         question: "Which HTML tag is used to create a hyperlink?",
+//         options: [
+//             "a",
+//             "link",
+//             "href",
+//             "hyperlink"
+//         ],
+//         answer: "a"
+//     },
+//     {
+//         id: 3,
+//         question: "Which CSS property is used to change the text color?",
+//         options: [
+//             "font-color",
+//             "text-color",
+//             "color",
+//             "background-color"
+//         ],
+//         answer: "color"
+//     },
+//     {
+//         id: 4,
+//         question: "Which CSS property controls the size of text?",
+//         options: [
+//             "text-style",
+//             "font-size",
+//             "text-size",
+//             "font-style"
+//         ],
+//         answer: "font-size"
+//     },
+//     {
+//         id: 5,
+//         question: "Inside which HTML element do we put JavaScript?",
+//         options: [
+//             "javascript",
+//             "js",
+//             "script",
+//             "code"
+//         ],
+//         answer: "script"
+//     },
+//     {
+//         id: 6,
+//         question: "Which symbol is used for single-line comments in JavaScript?",
+//         options: [
+//             "//",
+//             "/* */",
+//             "#",
+//         ],
+//         answer: "//"
+//     },
+//     {
+//         id: 7,
+//         question: "Which JavaScript keyword is used to declare a variable?",
+//         options: [
+//             "var",
+//             "int",
+//             "string",
+//             "declare"
+//         ],
+//         answer: "var"
+//     },
+//     {
+//         id: 8,
+//         question: "Which CSS property is used to make a website responsive?",
+//         options: [
+//             "flex",
+//             "media-query",
+//             "@media",
+//             "responsive"
+//         ],
+//         answer: "@media"
+//     },
+//     {
+//         id: 9,
+//         question: "Which method is used to select an element by its ID in JavaScript?",
+//         options: [
+//             "getElementByClass()",
+//             "getElementById()",
+//             "querySelectorAll()",
+//             "selectById()"
+//         ],
+//         answer: "getElementById()"
+//     },
+//     {
+//         id: 10,
+//         question: "Which HTML attribute is used to define inline styles?",
+//         options: [
+//             "class",
+//             "styles",
+//             "style",
+//             "font"
+//         ],
+//         answer: "style"
+//     }
+// ];
 
-var questionElement = document.getElementById('questionElement')
-var optionELement = document.getElementById('optionELement')
-var quizProgress = document.getElementById('quizProgress')
-var reportCardContainer = document.querySelector('.reportCardContainer')
-var nextQuesBtn = document.getElementById('nextQuesBtn')
-var quizContainer = document.querySelector('.quizContainer')
-var formContainer = document.getElementById('formContainer')
-var username = document.getElementById("username").value  
-var email = document.getElementById("email").value
-var userClass = document.getElementById("class").value
-var userhead = document.getElementById('userhead')
+// var questionElement = document.getElementById('questionElement')
+// var optionELement = document.getElementById('optionELement')
+// var quizProgress = document.getElementById('quizProgress')
+// var reportCardContainer = document.querySelector('.reportCardContainer')
+// var nextQuesBtn = document.getElementById('nextQuesBtn')
+// var quizContainer = document.querySelector('.quizContainer')
+// var formContainer = document.getElementById('formContainer')
+// var username = document.getElementById("username").value
+// var email = document.getElementById("email").value
+// var userClass = document.getElementById("class").value
+// var userhead = document.getElementById('userhead')
 
-var counter = 0;
-var totalCorrectAns = 0;
-
-
-function startQuiz(event) {
-  
-    if(event){
-        event.preventDefault();
-    }
-    quizContainer.style.display = 'block'
- userhead.innerText = "User: " + username
-
-    formContainer.style.display = 'none'
-    var question = quizQuestions[counter].question
-    var options = quizQuestions[counter].options
-    questionElement.innerHTML = question
-    optionELement.innerHTML = ''
-    for (var i = 0; i < options.length; i++) {
-        var li = ` <li onClick="checkAns(this)">${options[i]}</li>`
-        optionELement.innerHTML += li
-    }
-    quizProgress.innerHTML = `${counter + 1}/${quizQuestions.length}`
-    nextQuesBtn.setAttribute('disabled', true)
+// var counter = 0;
+// var totalCorrectAns = 0;
 
 
-}
-function nextQuestion() {
-    counter++
+// function startQuiz(event) {
 
-    if (counter < quizQuestions.length) {
-        startQuiz()
-        totalCorrectAns++
-    }
-    else {
-        alert('QuizEnd')
-        reportCardContainer.style.display = 'block'
-        quizContainer.style.display = 'none'
-        var reportCard = ` <h1>Report Card</h1>
-            <p>Total Question: ${quizQuestions.length}</p>
-            <p id="correctPara" >correct Answer:${totalCorrectAns}</p>
-            <p>Wrong Answer: ${quizQuestions.length - totalCorrectAns}</p>
-            <p>Grade: 100</p>`
-        reportCardContainer.innerHTML = reportCard
-    }
-}
-function checkAns(li) {
-    var correctAns = quizQuestions[counter].answer
-    var userAns = li.innerHTML
-    var allList = optionELement.getElementsByTagName("li")
-    var answer = quizQuestions[counter].answer
+//     if (event) {
+//         event.preventDefault();
+//     }
+//     quizContainer.style.display = 'block'
+//     userhead.innerText = "User: " + username
 
-    if (correctAns === userAns) {
+//     formContainer.style.display = 'none'
+//     var question = quizQuestions[counter].question
+//     var options = quizQuestions[counter].options
+//     questionElement.innerHTML = question
+//     optionELement.innerHTML = ''
+//     for (var i = 0; i < options.length; i++) {
+//         var li = ` <li onClick="checkAns(this)">${options[i]}</li>`
+//         optionELement.innerHTML += li
+//     }
+//     quizProgress.innerHTML = `${counter + 1}/${quizQuestions.length}`
+//     nextQuesBtn.setAttribute('disabled', true)
 
-        li.style.background = "green"
-    }
-    else {
-        li.style.background = "red"
-        for (var li of allList) {
-            if (li.innerHTML == correctAns) {
-                li.style.background = "green"
-                break;
-            }
-        }
-    }
 
-    for (var li of allList) {
-        li.style.pointerEvents = "none"
-    }
-    nextQuesBtn.removeAttribute('disabled')
-}
+// }
+// function nextQuestion() {
+//     counter++
+
+//     if (counter < quizQuestions.length) {
+//         startQuiz()
+//         totalCorrectAns++
+//     }
+//     else {
+//         alert('QuizEnd')
+//         reportCardContainer.style.display = 'block'
+//         quizContainer.style.display = 'none'
+//         var reportCard = `
+// <div class="reportCard">
+
+// <h1 class="reportTitle">Quiz Result</h1>
+
+// <div class="scoreCircle">
+//     <span>${totalCorrectAns}/${quizQuestions.length}</span>
+// </div>
+
+// <div class="reportDetails">
+
+// <div class="detailBox">
+// <p>Total Questions</p>
+// <h3>${quizQuestions.length}</h3>
+// </div>
+
+// <div class="detailBox correct">
+// <p>Correct Answers</p>
+// <h3>${totalCorrectAns}</h3>
+// </div>
+
+// <div class="detailBox wrong">
+// <p>Wrong Answers</p>
+// <h3>${quizQuestions.length - totalCorrectAns}</h3>
+// </div>
+
+// <div class="detailBox grade">
+// <p>Grade</p>
+// <h3>100</h3>
+// </div>
+
+// </div>
+
+// <button class="restartBtn" onclick="location.reload()">Restart Quiz</button>
+
+// </div>
+// `
+//         reportCardContainer.innerHTML = reportCard
+//     }
+// }
+// function checkAns(li) {
+//     var correctAns = quizQuestions[counter].answer
+//     var userAns = li.innerHTML
+//     var allList = optionELement.getElementsByTagName("li")
+//     var answer = quizQuestions[counter].answer
+
+//     if (correctAns === userAns) {
+
+//         li.style.background = "green"
+//     }
+//     else {
+//         li.style.background = "red"
+//         for (var li of allList) {
+//             if (li.innerHTML == correctAns) {
+//                 li.style.background = "green"
+//                 break;
+//             }
+//         }
+//     }
+
+//     for (var li of allList) {
+//         li.style.pointerEvents = "none"
+//     }
+//     nextQuesBtn.removeAttribute('disabled')
+// }
