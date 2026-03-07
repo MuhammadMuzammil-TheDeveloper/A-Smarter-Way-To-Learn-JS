e2e4405c6ba8ffcb32182f2cd11089498ece4d09
Muhammad Muzammil <muzans786@gmail.com>
2026-03-08 00:18:12 +0500

    ADD DOM

--- API/app.js
+++ API/app.js
@@ -1,6 +1,13 @@
-// API SYNTA
-fetch("https://opentdb.com/api.php?amount=5")
-.then(response => response.json())
-.then(data => {
-    console.log(data)
-})
+// // API SYNTA
+// fetch("https://opentdb.com/api.php?amount=5")
+// .then(response => response.json())
+// .then(data => {
+//     console.log(data)
+// })
+// .catch(function(error){
+//     console.error('Error', error)
+// })
+// Product API
+fetch('https://fakestoreapi.com/products/')
+    .then(res => res.json())
+    .then(json => console.log(json))
