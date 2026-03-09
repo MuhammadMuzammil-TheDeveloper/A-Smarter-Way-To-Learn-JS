570edbddf67ac5729986b5efe440be331f314ba0
Muhammad Muzammil <muzans786@gmail.com>
2026-03-09 19:42:19 +0500

    Add Arrow Function && Scope

--- Solutrions/Arrow_Function/app.js
+++ Solutrions/Arrow_Function/app.js
@@ -7,3 +7,14 @@
 // const greet = name => "Hello " + name;
 // console.log(greet("Muzammil"))
 
+// var marks = [29,73,48,74,92,100]
+
+// const checkGrade = marks.filter(mark => mark>40)
+// console.log(checkGrade)
+
+// function square(x){
+//  return x*x;
+// }
+
+// const sqaure = (x) => x*x;
+// console.log(sqaure(3))
---
+++ Solutrions/Scope/app.js
@@ -0,0 +1,29 @@
+// Scope means where variables can be accessed.
+
+// There are 3 types of scope.
+
+// 1️⃣ Global Scope
+// Variable declared outside functions.
+
+// let UserName = "Muhammad-Muzammil"
+// function show() {
+//     alert(`Hello `+ UserName)
+// }
+// show()
+
+// 2️⃣ Function Scope
+// Variable declared inside function.
+// function printAge(){
+//     let age = 19;
+//     console.log(age)
+// }
+// printAge()
+// console.log(age)
+
+// 3️⃣ Block Scope
+// Variables declared with let or const inside {}.
+// if(true){
+//  let x = 10;
+// }
+
+// console.log(x);
