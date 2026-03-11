b4fa5674256081ba491a56d3a6ae9924396a07f9
Muhammad Muzammil <muzans786@gmail.com>
2026-03-11 23:46:59 +0500

    For EACH()

---
+++ Solutrions/Array && Object/app.js
@@ -0,0 +1,15 @@
+// for each()
+
+// const fruits = ["apple", "orange", "cherry"];
+// fruits.forEach(myFunction);
+
+let number = [1,24,56,3]
+number.forEach(function(num) {
+    console.log(num)
+});
+
+let numbers = [1,2,3,4]
+
+numbers.forEach(function(num){
+    console.log(num)
+})
